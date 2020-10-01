70c91aceb1856a60074880867bc0b46535f348de
Derek Johnston <derekjohnstonlbk@gmail.com>
2020-10-01 15:34:54 -0500

    Adding styled components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-import styles from './styles.css';
+const Button = styled.button`
+  cursor: pointer;
+  background: transparent;
+  font-size: 16px;
+  border-radius: 3px;
+  color: palevioletred;
+  border: 2px solid palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  transition: 0.5s all ease-out;
+  
+  &:hover {
+    background-color: palevioletred;
+    color: white;
+  }
+`;
 
 const App = ({ title }) => (
-  <div className={styles.title}>{ title }</div>
+  <div>
+    { title }
+    <Button>I am a Button</Button>
+  </div>
 );
 
 App.propTypes = {
